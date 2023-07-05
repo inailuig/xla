732e8480a656a5dd96d08bc5335dcd436bd00cc3
Clemens Giuliani <clemens@inailuig.it>
2023-07-06 01:40:28 +0200

    update test; ok so now we are doing one mpi call per item, very inefficient

--- mpi_test/test.py
+++ mpi_test/test.py
@@ -4,13 +4,16 @@ import jax.numpy as jnp
 
 print(jax.devices())
 
-@partial(jax.jit, out_shardings=jax.sharding.PositionalSharding(jax.devices()))
+@partial(jax.jit, out_shardings=jax.sharding.PositionalSharding(jax.devices()).reshape(-1,1,1))
 def test(x):
     return x
-@jax.jit
-def s(x, y):
-    return x.sum(axis=-1), y.sum(axis=-1)
 
-x = test(jnp.ones(32))
-print(s(x, x))
+@jax.jit
+def s(x):
+    return x.sum(axis=0)
+
+
+x = jnp.ones((32, 5,3), dtype=jnp.float32)
+x = test(x)
+print(s(x))
 
