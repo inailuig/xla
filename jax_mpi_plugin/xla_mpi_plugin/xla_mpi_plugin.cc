5a2f961f12b12640a9edd0ac52f4e7c9de9c8a29
Clemens Giuliani <clemens@inailuig.it>
2023-11-29 17:39:16 +0100

    update for 4.20

--- jax_mpi_plugin/xla_mpi_plugin/xla_mpi_plugin.cc
+++ jax_mpi_plugin/xla_mpi_plugin/xla_mpi_plugin.cc
@@ -7,7 +7,7 @@
 namespace my_plugin {
 
 PJRT_Error* PJRT_Client_Create(PJRT_Client_Create_Args* args) {
-  PJRT_RETURN_IF_ERROR(pjrt::CheckMatchingStructSizes("PJRT_Client_Create_Args", PJRT_Client_Create_Args_STRUCT_SIZE,args->struct_size));
+  PJRT_RETURN_IF_ERROR(pjrt::ActualStructSizeIsGreaterOrEqual("PJRT_Client_Create_Args", PJRT_Client_Create_Args_STRUCT_SIZE,args->struct_size));
   // TODO(b/263170683): cpu_device_count should be configurable after config options can be passed to PJRT_Client_Create.
   PJRT_ASSIGN_OR_RETURN(std::unique_ptr<xla::PjRtClient> client,xla::GetTfrtCpuClientMPI(/*asynchronous=*/true));
   args->client = pjrt::CreateWrapperClient(std::move(client));
